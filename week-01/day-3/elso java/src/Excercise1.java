ce9476a6c6d108c00492694dda759112887a2ab0
Ferenczi Samu <farkas.david05@gmail.com>
2018-02-15 15:22:05 +0100

    excercises for Java day 1

--- week-01/day-3/elso java/src/Excercise1.java
+++ week-01/day-3/elso java/src/Excercise1.java
@@ -1,6 +1,6 @@
 public class Excercise1 {
-
     public static void main(String[] args) {
-        
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, Me!");
     }
 }
---
+++ week-01/day-3/elso java/src/Excercise3.java
@@ -0,0 +1,14 @@
+public class Excercise3 {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        String newline = System.getProperty("line.separator");
+        System.out.println("Hello, Esther!" + newline);
+        System.out.println("Hello, Mary!" + newline);
+        System.out.println("Hello, Joe!" + newline);
+    }
+}
