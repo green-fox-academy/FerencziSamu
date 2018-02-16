f51a8db40c2d3d92bd611770dc12a403e9dd7dec
Ferenczi Samu <farkas.david05@gmail.com>
2018-02-16 12:42:21 +0100

    Excercise23

---
+++ week-01/day-3/elso java/src/Excercise23.java
@@ -0,0 +1,23 @@
+
+public class Excercise23 {
+    public static void main(String[] args){
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        //String lie = "I won't cheat on the exam!";
+        //System.out.println(String lie, 100);
+
+        //String lie = "I won't cheat on the exam!";
+        //while (lie < 10) {
+        //    System.out.println(lie);
+        //    lie += 1;
+        //
+        // Nem az, de még hasznos lehet:
+        // String str = "I won't cheat on the exam!";
+        // System.out.println(str.replace(" ", "\n"));
+
+        for (int i = 1 ; i <= 100; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+    }
+}
