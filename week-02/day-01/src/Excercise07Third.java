968a488a29c763a5556099b82b01dfd1bbe0d7ad
Ferenczi Samu <farkas.david05@gmail.com>
2018-02-19 15:46:16 +0100

    Excercise 07

---
+++ week-02/day-01/src/Excercise07Third.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+
+public class Excercise07Third {
+
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+
+
+    public static void main(String[] args) {
+        Integer[] q = {4, 5, 6, 7};
+        System.out.println(q[2]);
+    }
+
+}
