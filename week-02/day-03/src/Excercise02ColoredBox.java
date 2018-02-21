5a5dbb3d9e51d0a1442968852c0d253bcfd99625
Ferenczi Samu <farkas.david05@gmail.com>
2018-02-21 11:08:03 +0100

    Excercise02ColoredBox

--- week-02/day-03/src/Excercise02ColoredBox.java
+++ week-02/day-03/src/Excercise02ColoredBox.java
@@ -1,17 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
-public class Excercise01LineInTheMiddle {
 
+public class Excercise02ColoredBox {
     public static void mainDraw(Graphics graphics) {
-        // draw a red horizontal line to the canvas' middle.
-        // draw a green vertical line to the canvas' middle.
+        // Draw a box that has different colored lines on each edge.
         graphics.setColor((Color.green));
-        graphics.drawLine(158, 0, 158, 343);
+        graphics.drawLine(10, 10, 150, 10);
         graphics.setColor((Color.red));
-        graphics.drawLine(0, 150, 320, 150);
-    }
+        graphics.drawLine(10, 10, 10, 150);
+        graphics.setColor((Color.yellow));
+        graphics.drawLine(10, 150, 150, 150);
+        graphics.setColor((Color.blue));
+        graphics.drawLine(150, 150, 150, 10);
 
+    }
 
     // Don't touch the code below
     static int WIDTH = 320;
