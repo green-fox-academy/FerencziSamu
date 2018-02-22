26089767cbfa4c24b5cfeae5c46a8c4883225de4
Ferenczi Samu <farkas.david05@gmail.com>
2018-02-22 13:51:39 +0100

    Excercise07MatchmakingfromDay2

--- week-02/day-02/src/Excercise07Matchmaking.java
+++ week-02/day-02/src/Excercise07Matchmaking.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Excercise07Matchmaking {
 
-        public static void main(String... args){
-            ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
-            ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
+    public static void main(String... args) {
+        List<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+        List<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
 
-            // Write a method that joins the two lists by matching one girl with one boy into a new list
-            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+        // Write a method that joins the two lists by matching one girl with one boy into a new list
+        // Expected output: "Eve", "Joe", "Ashley", "Fred"...
 
-            System.out.println(makingMatches(girls, boys));
+        System.out.println(makingMatches(girls, boys));
+    }
+
+    public static List<String> makingMatches(List<String> listOfGirls, List<String> listOfBoys) {
+        int position = 1;
+        if (listOfBoys.size() > listOfGirls.size()) {
+            for (int i = 0; i < listOfGirls.size(); i++) {
+                listOfBoys.add(position, listOfGirls.get(i));
+                position = position + 2;
+            }
+
         }
-        public static ArrayList<String> makingMatches (ArrayList<String> matched){
-            matched.toArray()
 
-        }
+        return listOfBoys;
+    }
+
 }
