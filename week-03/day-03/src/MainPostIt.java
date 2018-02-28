cb87fee223f6805cef4674b53953a7dd74a1d991
Ferenczi Samu <farkas.david05@gmail.com>
2018-02-28 11:35:45 +0100

    Excercise01PostIt

---
+++ week-03/day-03/src/MainPostIt.java
@@ -0,0 +1,23 @@
+public class MainPostIt {
+  public static void main(String [] args){
+    PostIt postit1 = new PostIt();
+    PostIt postit2 = new PostIt();
+    PostIt postit3 = new PostIt();
+
+
+
+    postit1.backgroundColor = "orange";
+    postit1.text = "Idea 1";
+    postit1.textColor = "blue";
+
+    postit2.backgroundColor = "pink";
+    postit2.text = "Awesome";
+    postit2.textColor = "black";
+
+    postit3.backgroundColor = "yellow";
+    postit3.text = "Superb";
+    postit3.textColor = "green";
+
+  }
+
+}
