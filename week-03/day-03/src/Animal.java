318879610b12d1d71af3a090a4ad2ebefabc3404
Ferenczi Samu <farkas.david05@gmail.com>
2018-02-28 15:38:21 +0100

    Excercise05FleetOfThings and Excercise06 Animal

--- week-03/day-03/src/Animal.java
+++ week-03/day-03/src/Animal.java
@@ -1,18 +1,22 @@
 public class Animal {
-    public int hunger = 50;
-    public int thirst = 50;
-
-    public Animal(){
+  private int hunger = 50;
+  private int thirst = 50;
 
 
+  public void eat() {
+    hunger--;
+  }
 
+  public void drink() {
+    thirst--;
+  }
 
+  public void play() {
+    hunger++;
+    thirst++;
+  }
 
-
-    }
-
-
-
-
-
+  public String toString() {
+    return "Hunger: " + hunger + "\nThirst: " + thirst;
+  }
 }
--- week-03/day-03/src/MainAnimal.java
+++ week-03/day-03/src/MainAnimal.java
@@ -1,6 +1,10 @@
 public class MainAnimal {
   public static void main(String[] args) {
+  Animal cat = new Animal();
+  cat.drink();
+  cat.play();
 
+    System.out.println(cat.toString());
 
 
 
