45b499fccf6561a846c44ec11866e8869f48f41b
Ferenczi Samu <farkas.david05@gmail.com>
2018-03-08 12:58:55 +0100

    TestSumDone

--- week-04/day-04/Tests/SumTest.java
+++ week-04/day-04/Tests/SumTest.java
@@ -10,12 +10,33 @@ public class SumTest {
   List<Integer> listOfNumbers = new ArrayList<>();
 
   @Test
-  public void sumTest() {
+  public void sumTestAdd() {
     listOfNumbers.add(1);
     listOfNumbers.add(2);
     listOfNumbers.add(3);
     listOfNumbers.add(4);
     listOfNumbers.add(5);
     assertEquals(15,summa.Sum(listOfNumbers));
+  }
+  @Test
+  public void sumTestEmptyList(){
+    assertTrue(summa.Sum(listOfNumbers) == 0);
+    //assertEquals(0,summa.Sum(listOfNumbers));
+  }
+  @Test
+  public void sumTestOneElement(){
+    listOfNumbers.add(5);
+    assertEquals(5,summa.Sum(listOfNumbers));
+  }
+  @Test
+  public void sumTestMultipleElements(){
+    listOfNumbers.add(5);
+    listOfNumbers.add(6);
+    assertEquals(11,summa.Sum(listOfNumbers));
+  }
+  @Test (expected = NullPointerException.class)
+  public void sumTestNull(){
+    listOfNumbers.add(null);
+    assertEquals(null,summa.Sum(listOfNumbers));
   }
 }
