24bf28c03756035ca780dfddcc56c870d547ceea
Ferenczi Samu <farkas.david05@gmail.com>
2018-03-08 11:10:13 +0100

    Test Apples 1

---
+++ week-04/day-04/Tests/ApplesTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplesTest {
+  Apples apple = new Apples();
+
+  @Test
+  public void getAppleTest() {
+    assertTrue("Apple",true);
+
+  }
+}
