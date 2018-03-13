a754b0f3ca078fb47540a5a6575836fa3d4a615c
Ferenczi Samu <farkas.david05@gmail.com>
2018-03-13 14:39:23 +0100

    FishTank skeletons of skeletons

---
+++ gyakorlas03/src/Fish.java
@@ -0,0 +1,14 @@
+// Each fish has a name, weight, color and has a status and feed method.
+public class Fish {
+  private String name;
+  private int weight;
+  private String colour;
+
+  public String getStatus() {
+    return name + ", weight: " + weight + ", colour: " + colour;
+  }
+  
+  public void feed() {
+
+  }
+}
