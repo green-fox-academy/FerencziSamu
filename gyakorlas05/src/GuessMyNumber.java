566db2af207d96ccf6aff92c32ec5f8d082fccf2
Ferenczi Samu <farkas.david05@gmail.com>
2018-03-14 16:28:24 +0100

    GuessMyNumber skeleton and more fun

--- gyakorlas05/src/GuessMyNumber.java
+++ gyakorlas05/src/GuessMyNumber.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 
 public class GuessMyNumber {
   public static void main(String[] args) {
-    int numberOfComputer = (int) Math.random() * 100;
+    int numberOfComputer = (int) (Math.random() * 100);
     Scanner scanner = new Scanner(System.in);
+//    System.out.println(numberOfComputer);
     System.out.println("Enter a guess!");
+
     int guess = scanner.nextInt();
 
-    if (guess > numberOfComputer) {
-      System.out.println("Too high!");
-    } else if (guess < numberOfComputer) {
-      System.out.println("Too low!");
-    } else {
-      System.out.println("Congratulations! You won!");
+    while (guess != numberOfComputer) {
+      if (guess > numberOfComputer) {
+        System.out.println("Too high! Guess again!");
+        guess = scanner.nextInt();
+      } else if (guess < numberOfComputer) {
+        System.out.println("Too low! Guess again!");
+        guess = scanner.nextInt();
+      }
     }
+    System.out.println("Congratulations! You won!");
   }
 }
