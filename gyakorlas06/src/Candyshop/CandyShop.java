f0720331c6ab95faa3e0b7937bca0b452129b29b
Ferenczi Samu <farkas.david05@gmail.com>
2018-03-22 15:01:25 +0100

    Candyshop update

--- gyakorlas06/src/Candyshop/CandyShop.java
+++ gyakorlas06/src/Candyshop/CandyShop.java
@@ -1,4 +1,11 @@
 package Candyshop;
 
 public class CandyShop  {
+  private int gramsOfSugar;
+  public static final Lollipop LOLLIPOP = new Lollipop();
+  public static final Candy CANDY = new Candy();
+
+  public CandyShop (int gramsOfSugar) {
+    this.gramsOfSugar = gramsOfSugar;
+  }
 }
--- gyakorlas06/src/Candyshop/TakeMeToThe.java
+++ gyakorlas06/src/Candyshop/TakeMeToThe.java
@@ -20,7 +20,7 @@ public class TakeMeToThe {
     // If we buy sugar we can raise the CandyShop's amount of sugar and reduce the income by the price of it.
     // The CandyShop should be represented as string in this format:
     // "Inventory: 3 candies, 2 lollipops, Income: 100, Sugar: 400gr"
-    candyShop = new CandyShop(300);
+    CandyShop candyShop = new CandyShop(300);
     candyShop.createSweets(CandyShop.CANDY);
     candyShop.createSweets(CandyShop.CANDY);
     candyShop.createSweets(CandyShop.LOLLIPOP);
@@ -37,7 +37,7 @@ public class TakeMeToThe {
     System.out.println(candyShop);
     // Should print out:
     // "Invetory: 1 candies, 1 lollipops, Income: 35$, Sugar: 285gr"
-    candyShop.buySugar(300);
+    CandyShop.buySugar(300);
     System.out.println(candyShop);
     // Should print out:
     // "Invetory: 1 candies, 1 lollipops, Income: 5$, Sugar: 315gr"
