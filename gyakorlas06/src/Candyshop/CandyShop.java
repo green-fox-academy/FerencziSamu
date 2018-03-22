cd0629de3e871f54e3f18cff466b95ea3eb81cde
Ferenczi Samu <farkas.david05@gmail.com>
2018-03-22 17:36:35 +0100

    candy in progress

--- gyakorlas06/src/Candyshop/CandyShop.java
+++ gyakorlas06/src/Candyshop/CandyShop.java
@@ -1,11 +1,23 @@
 package Candyshop;
 
-public class CandyShop  {
+import java.util.ArrayList;
+
+
+public class CandyShop {
+  ArrayList<Candy> shop = new ArrayList<>();
   private int gramsOfSugar;
   public static final Lollipop LOLLIPOP = new Lollipop();
   public static final Candy CANDY = new Candy();
 
-  public CandyShop (int gramsOfSugar) {
+  public CandyShop(int gramsOfSugar) {
     this.gramsOfSugar = gramsOfSugar;
+  }
+
+  public void createSweets(Candy candy) {
+    shop.add(candy);
+  }
+
+  public void createSweets(Lollipop lollipop) {
+
   }
 }
