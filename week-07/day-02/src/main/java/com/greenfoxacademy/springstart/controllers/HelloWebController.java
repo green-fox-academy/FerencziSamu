11ade2bf12564fc114633ace8ea1a406adcf5d45
Ferenczi Samu <farkas.david05@gmail.com>
2018-04-03 15:39:34 +0200

    amazingly works

--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,15 +3,19 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @Controller
 public class HelloWebController {
+  AtomicLong counter = new AtomicLong(0);
 
   @RequestMapping(value = "/web/greeting")
-  public String greeting(Model model) {
+  public String greeting(Model model, @RequestParam("name") String name) {
     model.addAttribute("name", " World");
+    model.addAttribute("counter", counter.addAndGet(1));
     return "greeting";
   }
-
 }
