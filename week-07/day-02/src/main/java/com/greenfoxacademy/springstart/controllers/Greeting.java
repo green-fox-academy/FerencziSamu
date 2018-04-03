65c75f1b26870158e7c4378f7b5b32ecf78e6840
Ferenczi Samu <farkas.david05@gmail.com>
2018-04-03 15:33:06 +0200

    greeting.html betoltve

--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -1,11 +1,13 @@
 package com.greenfoxacademy.springstart.controllers;
 
+
 public class Greeting {
   long id;
   String content;
 
 
-  public Greeting(long id,String content) {
+
+  public Greeting(long id, String content) {
     this.id = id;
     this.content = content;
   }
--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+
 @RestController
 public class HelloRESTController {
+  AtomicLong id = new AtomicLong(0);
+
   @RequestMapping(value = "greeting")
-  public Greeting Greeting(@RequestParam("name")String name) {
-    return new Greeting(1, "Hello," + name + "!");
+  public Greeting Greeting(@RequestParam("name") String name) {
+    return new Greeting(id.addAndGet(1), "Hello," + name + "!");
   }
 }
