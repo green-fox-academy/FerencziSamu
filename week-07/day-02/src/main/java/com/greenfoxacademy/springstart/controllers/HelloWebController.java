c5b2de3f129546b629d1b00c22632e8155cc6606
Ferenczi Samu <farkas.david05@gmail.com>
2018-04-03 16:55:01 +0200

    no szinek,de a többi királyosban vereti a koszonesit

--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -10,12 +10,26 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  String[] hellos = {"Jó napot", "Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den",
+      "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
   AtomicLong counter = new AtomicLong(0);
 
   @RequestMapping(value = "/web/greeting")
   public String greeting(Model model, @RequestParam("name") String name) {
-    model.addAttribute("name", " World");
+    model.addAttribute("name", name);
     model.addAttribute("counter", counter.addAndGet(1));
     return "greeting";
   }
+
+  @RequestMapping(value = "/web/hello")
+  public String greetings(Model model, @RequestParam("name") String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("greetings", hellos);
+    return "hello";
+  }
 }
+
+
