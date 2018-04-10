24b9b3bd6d342c3eecd688e2c41d01b1266811a1
Ferenczi Samu <farkas.david05@gmail.com>
2018-04-10 17:41:42 +0200

    added add page and tried to create new but removed it as i remembered i need to create new objects in factory but later on realized i dont have any factory. yet.

--- week-08/day-02/Connection with MySQL/src/main/java/com/greenfox/sql_fun/controllers/TodoController.java
+++ week-08/day-02/Connection with MySQL/src/main/java/com/greenfox/sql_fun/controllers/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,9 +31,14 @@ public class TodoController {
     return "todosList";
   }
 
-  @RequestMapping(value = "/add")
+  @PostMapping(value = "/add")
   public String addingNewTodo(Model model, @ModelAttribute(value = "title")String title) {
     model.addAttribute("title",title);
     return "add";
+  }
+
+  @RequestMapping(value = "/add")
+  public String addingNewTodo(){
+    return "redirect:todoList";
   }
 }
--- week-08/day-02/Connection with MySQL/src/main/java/com/greenfox/sql_fun/repositories/TodoInterface.java
+++ week-08/day-02/Connection with MySQL/src/main/java/com/greenfox/sql_fun/repositories/TodoInterface.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface TodoInterface extends CrudRepository<Todo, Long> {
+  
 }
