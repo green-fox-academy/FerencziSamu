09272066816448c9267cf455f7383c3dfcd21792
Ferenczi Samu <farkas.david05@gmail.com>
2018-04-10 11:47:05 +0200

    just started again

---
+++ week-08/day-02/Listing Todos H2/src/main/java/com/greenfox/h2/Controllers/TodoController.java
@@ -0,0 +1,18 @@
+package com.greenfox.h2.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/todo")
+public class TodoController {
+
+  @RequestMapping(value = {"/","/list"})
+  @ResponseBody
+  public String todoList(Model model) {
+    return "This is my first todo";
+  }
+
+}
--- week-08/day-02/Listing Todos H2/src/main/java/com/greenfox/h2/Todo.java
+++ week-08/day-02/Listing Todos H2/src/main/java/com/greenfox/h2/Todo.java
@@ -1,5 +1,10 @@
 package com.greenfox.h2;
 
 public class Todo {
-  
+
+  private long id;
+  private String title;
+  private boolean urgent = false;
+  private boolean done = false;
+
 }
