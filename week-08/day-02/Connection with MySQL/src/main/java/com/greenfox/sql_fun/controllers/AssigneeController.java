a72869f86faa33592754caa9f21da99bafc2325f
Ferenczi Samu <farkas.david05@gmail.com>
2018-04-13 10:46:07 +0200

    assignees mapped

--- week-08/day-02/Connection with MySQL/src/main/java/com/greenfox/sql_fun/controllers/AssigneeController.java
+++ week-08/day-02/Connection with MySQL/src/main/java/com/greenfox/sql_fun/controllers/AssigneeController.java
@@ -1,7 +1,26 @@
 package com.greenfox.sql_fun.controllers;
 
+import com.greenfox.sql_fun.repositories.AssigneeRepository;
+import com.greenfox.sql_fun.services.AssigneeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/todo/assignees")
 public class AssigneeController {
+
+  @Autowired
+  AssigneeRepository assigneeRepository;
+
+  @Autowired
+  AssigneeService assigneeService;
+
+  @RequestMapping(value = {"/", "/list"})
+  public String listOfAssignees(Model model) {
+    model.addAttribute("assignees",assigneeRepository.findAll());
+    return "assigneesList";
+  }
+
 }
